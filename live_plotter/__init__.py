ac047617ad47df313b2cf7f803a24f7eadf012dc
Tyler Lum <tylergwlum@gmail.com>
2023-11-19 22:05:33 -0800

    Add live_image_plotter_grid_separate_process_core.py live_plotter_grid_separate_process_core.py

--- live_plotter/__init__.py
+++ live_plotter/__init__.py
@@ -1,8 +1,12 @@
 from .live_plotter_core import LivePlotter, LivePlotterGrid
 from .fast_live_plotter_core import FastLivePlotter, FastLivePlotterGrid
+from .live_plotter_grid_separate_process_core import LivePlotterGridSeparateProcess
 from .fast_live_plotter_grid_separate_process_core import FastLivePlotterGridSeparateProcess
 from .live_image_plotter_core import LiveImagePlotter, LiveImagePlotterGrid
 from .fast_live_image_plotter_core import FastLiveImagePlotter, FastLiveImagePlotterGrid
+from .live_image_plotter_grid_separate_process_core import (
+    LiveImagePlotterGridSeparateProcess,
+)
 from .fast_live_image_plotter_grid_separate_process_core import (
     FastLiveImagePlotterGridSeparateProcess,
 )
